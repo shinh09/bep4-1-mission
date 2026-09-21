b5b1df99323fefb73ec80ff8eb11a42c2a9925b2
shinhyung <23102009@seoultech.ac.kr>
2026-09-21 15:21:44 +0900

    0015 - POST_MEMBER 테이블 생성, 아직 본격적인 도입 전

--- src/main/java/com/back/boundedContext/member/domain/Member.java
+++ src/main/java/com/back/boundedContext/member/domain/Member.java
@@ -3,10 +3,12 @@ package com.back.boundedContext.member.domain;
 import com.back.global.jpa.entity.BaseIdAndTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "MEMBER_MEMBER")
 @NoArgsConstructor
 @Getter
 public class Member extends BaseIdAndTime {
@@ -21,6 +23,7 @@ public class Member extends BaseIdAndTime {
         this.password = password;
         this.nickname = nickname;
     }
+
     public int increaseActivityScore(int amount) {
         return this.activityScore += amount;
     }
