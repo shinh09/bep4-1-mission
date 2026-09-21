4bc1b292b859453a4e700c9209f65fd6e1b8d598
shinhyung <23102009@seoultech.ac.kr>
2026-09-21 23:26:38 +0900

    0019 - syncMember 메서드 리팩토링, PostMember 클래스 생성자가 id, createDate, modifyDate 칼럼도 같이 받기

--- src/main/java/com/back/shared/member/domain/ReplicaMember.java
+++ src/main/java/com/back/shared/member/domain/ReplicaMember.java
@@ -4,21 +4,24 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
+
 @NoArgsConstructor
-@Setter
 public abstract class ReplicaMember extends BaseMember {
     @Id
     private int id;
     private LocalDateTime createDate;
     private LocalDateTime modifyDate;
 
-    public ReplicaMember(String username, String password, String nickname) {
+    public ReplicaMember(int id, LocalDateTime createDate, LocalDateTime modifyDate, String username, String password, String nickname) {
         super(username, password, nickname);
+        this.id = id;
+        this.createDate = createDate;
+        this.modifyDate = modifyDate;
     }
 }
