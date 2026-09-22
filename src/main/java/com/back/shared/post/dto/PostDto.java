d057cfd9d3f06d578aef4172bc67360e08b047bf
shinhyung <23102009@seoultech.ac.kr>
2026-09-22 23:43:37 +0900

    0040 - 인터페이스 도입하여 규격을 코드로 드러내기

--- src/main/java/com/back/shared/post/dto/PostDto.java
+++ src/main/java/com/back/shared/post/dto/PostDto.java
@@ -1,17 +1,14 @@
 package com.back.shared.post.dto;
 
-
-
+import com.back.standard.modelType.HasModelTypeCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
-
-
 @Getter
-public class PostDto {
+public class PostDto implements HasModelTypeCode {
     private final int id;
     private final LocalDateTime createDate;
     private final LocalDateTime modifyDate;
@@ -20,4 +17,8 @@ public class PostDto {
     private final String title;
     private final String content;
 
+    @Override
+    public String getModelTypeCode() {
+        return "Post";
+    }
 }
