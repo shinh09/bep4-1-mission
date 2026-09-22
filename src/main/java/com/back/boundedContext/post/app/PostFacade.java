08be7aa504e52ff1f10df8cfe6e7c8c0b3922f7d
shinhyung <23102009@seoultech.ac.kr>
2026-09-22 14:22:22 +0900

    0027 - cash 모듈 리팩토링, CashEventListener에 있는 비지니스 로직을 이벤트를 사용하여 제거

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -1,10 +1,9 @@
 package com.back.boundedContext.post.app;
 
-
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.domain.PostMember;
-import com.back.boundedContext.post.out.PostMemberRepository;
-import com.back.boundedContext.post.out.PostRepository;
+
+
 import com.back.global.rsData.RsData;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
@@ -16,13 +15,13 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class PostFacade {
-    private final PostRepository postRepository;
-    private final PostMemberRepository postMemberRepository;
+    private final PostSupport postSupport;
+    private final PostSyncMemberUseCase postSyncMemberUseCase;
     private final PostWriteUseCase postWriteUseCase;
 
-    @Transactional(readOnly = true)
-    public long count() {
-        return postRepository.count();
+    @Transactional
+    public PostMember syncMember(MemberDto member) {
+        return postSyncMemberUseCase.syncMember(member);
     }
 
     @Transactional
@@ -31,27 +30,19 @@ public class PostFacade {
     }
 
     @Transactional(readOnly = true)
-    public Optional<Post> findById(int id) {
-        return postRepository.findById(id);
+    public long count() {
+        return postSupport.count();
     }
 
-    @Transactional
-    public PostMember syncMember(MemberDto member) {
-        PostMember _member = new PostMember(
-                member.getId(),
-                member.getCreateDate(),
-                member.getModifyDate(),
-                member.getUsername(),
-                "",
-                member.getNickname(),
-                member.getActivityScore()
-        );
+    @Transactional(readOnly = true)
+    public Optional<Post> findById(int id) {
+        return postSupport.findById(id);
 
-        return postMemberRepository.save(_member);
+
     }
 
     @Transactional(readOnly = true)
     public Optional<PostMember> findMemberByUsername(String username) {
-        return postMemberRepository.findByUsername(username);
+        return postSupport.findMemberByUsername(username);
     }
 }
